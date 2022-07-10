515b2a465f6c4762cb9effe3015626266975f8c7
Desiree Armojallas <desiree.armojallas.20@usjr.edu.ph>
2022-07-10 09:08:42 +0800

    Service Locator and Services with GetIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pages_and_popups/services/notes_service.dart';
 import 'package:pages_and_popups/views/note_list.dart';
 
+void setupLocator() {
+  GetIt.I.registerLazySingleton(() => NotesService());
+}
+
 void main() {
-  runApp(const MyApp());
+  setupLocator();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +20,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: NoteList(),
--- lib/views/note_list.dart
+++ lib/views/note_list.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:pages_and_popups/models/note_for_listing.dart';
+import 'package:pages_and_popups/services/notes_service.dart';
 import 'package:pages_and_popups/views/note_delete.dart';
 import 'package:pages_and_popups/views/note_modify.dart';
 
-class NoteList extends StatelessWidget {
-  final notes = [
-    NoteForListing(
-        noteID: '1',
-        createDateTime: DateTime.now(),
-        latestEditDateTime: DateTime.now(),
-        noteTitle: 'Note 1'),
-    NoteForListing(
-        noteID: '2',
-        createDateTime: DateTime.now(),
-        latestEditDateTime: DateTime.now(),
-        noteTitle: 'Note 2'),
-    NoteForListing(
-        noteID: '3',
-        createDateTime: DateTime.now(),
-        latestEditDateTime: DateTime.now(),
-        noteTitle: 'Note 3'),
-  ];
+class NoteList extends StatefulWidget {
+  @override
+  State<NoteList> createState() => _NoteListState();
+}
 
+class _NoteListState extends State<NoteList> {
+  NotesService get service => GetIt.I<NotesService>();
+  List<NoteForListing> notes = [];
   String formatDateTime(DateTime dateTime) {
     return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
+  }
+
+  @override
+  void initState() {
+    notes = service.getNotesList();
+    super.initState();
   }
 
   @override
@@ -45,20 +42,19 @@ class NoteList extends StatelessWidget {
           return Dismissible(
             key: ValueKey(notes[index].noteID),
             direction: DismissDirection.startToEnd,
-            onDismissed: (direction) {
-
-            },
+            onDismissed: (direction) {},
             confirmDismiss: (direction) async {
               final result = await showDialog(
-                context: context,
-                builder: (context) => NoteDelete()
-              );
+                  context: context, builder: (context) => NoteDelete());
               return result;
             },
             background: Container(
               color: Colors.red,
               padding: EdgeInsets.only(left: 16),
-              child: Align(child: Icon(Icons.delete, color: Colors.white), alignment: Alignment.centerLeft,),
+              child: Align(
+                child: Icon(Icons.delete, color: Colors.white),
+                alignment: Alignment.centerLeft,
+              ),
             ),
             child: ListTile(
               title: Text(
@@ -68,8 +64,9 @@ class NoteList extends StatelessWidget {
               subtitle: Text(
                   'Last edited on ${formatDateTime(notes[index].latestEditDateTime)}'),
               onTap: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => NoteModify(noteID: notes[index].noteID)));
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        NoteModify(noteID: notes[index].noteID)));
               },
             ),
           );
