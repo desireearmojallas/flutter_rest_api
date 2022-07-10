171bd79363743faefebf32232524f7a9ea6fdf36
Desiree Armojallas <desiree.armojallas.20@usjr.edu.ph>
2022-07-10 14:40:59 +0800

    Fetch a single item using GET method

--- lib/services/notes_service.dart
+++ lib/services/notes_service.dart
@@ -2,33 +2,38 @@ import 'dart:convert';
 import 'package:pages_and_popups/models/note_for_listing.dart';
 import 'package:http/http.dart' as http;
 import '../models/api_response.dart';
+import '../models/note.dart';
 
 class NotesService {
   static const api = 'https://tq-notes-api-jkrgrdggbq-el.a.run.app';
   static const headers = {'apiKey': '2e661342-ef5b-4758-b7c0-8ec5d4b8058f'};
 
   Future<APIResponse<List<NoteForListing>>> getNotesList() {
-      return http.get(Uri.parse('$api/notes'), headers: headers).then((data) {
+    return http.get(Uri.parse('$api/notes'), headers: headers).then((data) {
       if (data.statusCode == 200) {
         final jsonData = json.decode(data.body);
         final notes = <NoteForListing>[];
         for (var item in jsonData) {
-          final note = NoteForListing(
-            noteID: item['noteID'],
-            noteTitle: item['noteTitle'],
-            createDateTime: DateTime.parse(item['createDateTime']),
-            latestEditDateTime: item['latestEditDateTime'] != null
-                ? DateTime.parse(item['latestEditDateTime'])
-                : null,
-          );
-          notes.add(note);
+          notes.add(NoteForListing.fromJson(item));
         }
         return APIResponse<List<NoteForListing>>(data: notes);
       }
       return APIResponse<List<NoteForListing>>(
           error: true, errorMessage: 'An error occured');
-    })
-    .catchError((_) => APIResponse<List<NoteForListing>>(
-          error: true, errorMessage: 'An error occured'));
+    }).catchError((_) => APIResponse<List<NoteForListing>>(
+        error: true, errorMessage: 'An error occured'));
+  }
+
+  Future<APIResponse<Note>> getNote(String noteID) {
+    return http
+        .get(Uri.parse('$api/notes/$noteID'), headers: headers)
+        .then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        return APIResponse<Note>(data: Note.fromJson(jsonData));
+      }
+      return APIResponse<Note>(error: true, errorMessage: 'An error occured');
+    }).catchError((_) =>
+            APIResponse<Note>(error: true, errorMessage: 'An error occured'));
   }
 }
--- lib/views/note_modify.dart
+++ lib/views/note_modify.dart
@@ -1,10 +1,53 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pages_and_popups/services/notes_service.dart';
 
-class NoteModify extends StatelessWidget {
+import '../models/note.dart';
+
+class NoteModify extends StatefulWidget {
   final String? noteID;
-  bool get isEditing => noteID != null;
+  const NoteModify({Key? key, this.noteID}) : super(key: key);
 
-  NoteModify({this.noteID});
+  @override
+  State<NoteModify> createState() => _NoteModifyState();
+}
+
+class _NoteModifyState extends State<NoteModify> {
+  bool get isEditing => widget.noteID != null;
+
+  NotesService get notesService => GetIt.I<NotesService>();
+
+  String? errorMessage;
+  late Note note;
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+
+    setState(() {
+      _isLoading = true;
+    });
+    notesService.getNote(widget.noteID!).then((response) {
+      setState(() {
+      _isLoading = false;
+      });
+
+      if (response.error) {
+        errorMessage = response.errorMessage ?? 'An error occured';
+      }
+      note = response.data!;
+      _titleController.text = note.noteTitle!;
+      _contentController.text = note.noteContent!;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +55,17 @@ class NoteModify extends StatelessWidget {
       appBar: AppBar(title: Text(isEditing ? 'Edit note' : 'Create note')),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
+        child: _isLoading ? Center(child: CircularProgressIndicator()) : Column(
           children: <Widget>[
             TextField(
+              controller: _titleController,
               decoration: InputDecoration(hintText: 'Note title'),
             ),
             Container(
               height: 8,
             ),
             TextField(
+              controller: _contentController,
               decoration: InputDecoration(hintText: 'Note content'),
             ),
             Container(height: 16),
@@ -36,8 +81,7 @@ class NoteModify extends StatelessWidget {
                 onPressed: () {
                   if (isEditing) {
                     //update note in api
-                  }
-                  else {
+                  } else {
                     //create note in api
                   }
                   Navigator.of(context).pop();
